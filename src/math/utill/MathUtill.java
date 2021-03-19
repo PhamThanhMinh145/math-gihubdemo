a585f5f37274a3f7d884659500aa3ca1fbcab49e
PhamThanhMinh145 <thanhminh1452000@gmail.com>
2021-03-20 00:01:54 +0700

    1st upload to remote GitHub

---
+++ src/math/utill/MathUtill.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.utill;
+
+/**
+ *
+ * @author DELL
+ */
+public class MathUtill {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          System.out.println("Hello GitHub!");
+        System.out.println("This is  lst update from local to remote GITHUB");
+    }
+    
+}
